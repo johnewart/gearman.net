8f175b6643979b7164ccb4e42aa740af6bbed3d5
John Ewart <john@unixninjas.org>
2009-08-04 14:45:00 -0700

    [project @ ac866c608c9f370161f7e885180ea4868a6444da] Added constructors for passing data, packet type and jobhandle

--- src/RequestPacket.cs
+++ src/RequestPacket.cs
@@ -12,5 +12,24 @@ namespace Gearman
 			this.magic = "\0REQ".ToCharArray();
 			this.size = 0; 
 		}
+		
+		public RequestPacket(PacketType type) : this()
+		{
+			this.Type = type;
+		}
+		
+		public RequestPacket(PacketType type, byte[] data) : this(type)
+		{
+			this.Data = data;
+		}
+		
+		public RequestPacket(PacketType type, byte[] data, string jobhandle) 
+		{
+			// Set jobhandle and type first to parse data properly
+			this.JobHandle = jobhandle;
+			this.Type = type;
+			this.Data = data; 
+			
+		}
 	}
 }
